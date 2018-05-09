8e9b1583bdc378d758d437fcdb0544415d23a65d
Stanciu Alin <stanciualin26@yahoo.com>
2018-05-09 19:27:06 +0300

    Name
    a

--- generator.c
+++ generator.c
@@ -5,12 +5,6 @@
 
 int generate_number(int max_value) {
 
-  time_t t;
-
-  /* Intializes random number generator */
-  srand((unsigned) time(NULL));
-
-  /* Print 5 random numbers from 0 to 49 */
   return rand() % (max_value + 1);
 
 }
